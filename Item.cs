dc51349d15f7ca657d3f73a8006139cb7d47af55
DjpandaLamp <68932550+DjpandaLamp@users.noreply.github.com>
2024-10-30 11:15:28 -0700

    Added Items and Inventory Management
    Items and Inventory Management scripts have been added to create a dynamic and easy to use system of items

---
+++ Item.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ItemTag
+{
+    ArmorHead,
+    ArmorSocks,
+    ArmorShoes,
+    ArmorPants,
+    ArmorGloves,
+    ArmorRing,
+    ArmorHelmet,
+    ArmorShirt,
+    ConsumableHeal,
+    ConsumableStatus,
+    Weapon
+}
+[CreateAssetMenu(menuName = "Scriptable Objects/Item")]
+public class Item : ScriptableObject
+{
+    public int iD;
+    public ItemTag ItemTag;
+    public int ArmorValue;
+    public int ArmorRestist;
+
+    public int healthValue;
+    public int status;
+}
